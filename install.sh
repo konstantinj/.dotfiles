9318e497e54aa4b467a5a4ae5d94c2a9f08ba781
Konstantin Jakobi <kjakobi@goodgamestudios.com>
2017-09-28 14:27:20 +0200

    added reqs to install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,24 @@
-#!/bin/bash
+#!/bin/zsh
+
+if [ ! -d ~/.oh-my-zsh ]; then
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
+if [ ! -d ~/.oh-my-zsh/custom/plugins/zsh-completions ]; then
+	git clone https://github.com/zsh-users/zsh-completions.git ~/.oh-my-zsh/custom/plugins/zsh-completions
+fi
+
+if [ ! -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+fi
+
+if [ ! -d ~/.oh-my-zsh/custom/themes/powerlevel9k ]; then
+	git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+fi
+
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
 
 time=$(date +%s)
 files=( ".vimrc" ".zshrc" )
@@ -14,3 +34,5 @@ for file in "${files[@]}"; do
   fi
   ln -sn $new_file $cur_file
 done
+
+vim +PluginInstall +qall
